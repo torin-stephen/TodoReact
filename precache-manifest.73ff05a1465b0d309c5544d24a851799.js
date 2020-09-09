ee489594f0e99d08bbd5c5644e872dd6d46ef6cf
Torin Stephen <tkmsmc@gmail.com>
2020-09-09 16:59:41 +0100

    Updates

---
+++ precache-manifest.73ff05a1465b0d309c5544d24a851799.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f02f22051266f472216e94d160da1ed",
+    "url": "/torin-stephen/TodoReact/index.html"
+  },
+  {
+    "revision": "7c36988ca3c2c855146c",
+    "url": "/torin-stephen/TodoReact/static/css/main.92bd934a.chunk.css"
+  },
+  {
+    "revision": "053032ad0fc94857740a",
+    "url": "/torin-stephen/TodoReact/static/js/2.aac1ab34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/torin-stephen/TodoReact/static/js/2.aac1ab34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c36988ca3c2c855146c",
+    "url": "/torin-stephen/TodoReact/static/js/main.6551b7ef.chunk.js"
+  },
+  {
+    "revision": "2ce3c0e37ed365913630",
+    "url": "/torin-stephen/TodoReact/static/js/runtime-main.042e77a4.js"
+  }
+]);
